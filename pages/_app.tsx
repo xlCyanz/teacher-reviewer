dfb83a00718d0aa4dee235466bcf4f9276793b94
Heger Arias Santos <hegel.0218@gmail.com>
2022-01-12 08:53:00 -0400

    comenzare a hacer el login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import MainLayout from "@components/main-layout";
+// import MainLayout from "@components/main-layout";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 const Application = ({ Component, pageProps }: AppProps) => (
-  <MainLayout>
-    <Component {...pageProps} />
-  </MainLayout>
+  // <MainLayout>
+  <Component {...pageProps} />
+  // </MainLayout>
 );
 
 export default Application;
