bebfb09a88bc2ce62188d85b29a9911b425be255
xlCyanz <johanse.linares@gmail.com>
2022-01-21 17:03:14 -0400

    New alias for global types.

---
+++ types/index.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export interface PropertyTabs {
+  name: string;
+  href: string;
+}
+
+export interface ResponseFuncs {
+  GET?: (request: NextApiRequest, response: NextApiResponse) => Promise<void>;
+  POST?: (request: NextApiRequest, response: NextApiResponse) => Promise<void>;
+  PUT?: (request: NextApiRequest, response: NextApiResponse) => Promise<void>;
+  DELETE?: (request: NextApiRequest, response: NextApiResponse) => Promise<void>;
+}
+
+export interface Vote {
+  userName: string;
+  scoreClarity: number;
+  scoreAssistance: number;
+  scoreTakeClassAgain: number;
+}
+
+export interface Teacher {
+  id: string;
+  name: string;
+  area: string;
+  votes?: Vote[];
+}
+
+export interface Comment {
+  id?: string;
+  userId?: string;
+  teacherId?: string;
+  updatedAt?: string;
+  createdAt?: string;
+  body?: string;
+}
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  avatar: string;
+  password?: string;
+}
+
+export interface EnviromentVariables {
+  databaseUrl?: string | null;
+  graphqlUrl?: string | null;
+  googleClientId? : string | null;
+}
