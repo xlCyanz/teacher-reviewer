f7bcfe7e6cc709e0aa5444885b6b6bd2b0fbaf7a
xlCyanz <johanse.linares@gmail.com>
2022-01-26 23:19:10 -0400

    Teacher details page update.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 import "../styles/globals.css";
 
 const Application = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
   <SessionProvider session={session}>
-    <Component {...pageProps} />
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+
+    </ApolloProvider>
   </SessionProvider>
 );
 export default Application;
