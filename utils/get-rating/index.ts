a44181f3ec1fe60e5c08da70740ef6fbb0fa8830
xlCyanz <johanse.linares@gmail.com>
2022-02-11 19:30:47 -0400

    Updated utility to get rating.

--- utils/get-rating/index.ts
+++ utils/get-rating/index.ts
@@ -1,44 +1,35 @@
 import { IVote } from "@types";
 
-interface PropsChangeVariablesNames {
-    assistence: number;
-    classAgain: number;
-    clarity: number;
-}
-
-const changeVariablesNames = ({ assistence, classAgain, clarity }: PropsChangeVariablesNames) => ({
-  scoreAssistance: assistence,
-  scoreTakeClassAgain: classAgain,
-  scoreClarity: clarity,
-});
-
-const getRating = (votes: IVote[] | undefined) => {
+const getRating = (votes: IVote[]) => {
   if (votes === undefined || !votes[0]) {
-    return changeVariablesNames(
-      {
-        assistence: 0,
-        classAgain: 0,
-        clarity: 0,
-      },
-    );
+    return {
+      scoreAssistance: 0,
+      scoreTakeClassAgain: 0,
+      scoreClarity: 0,
+    };
   }
 
   let assistence = 0;
   let classAgain = 0;
   let clarity = 0;
 
-  for (let index = 0; index < votes.length; index += 1) {
-    const { scoreAssistance, scoreTakeClassAgain, scoreClarity } = votes[index];
+  votes.forEach((vote) => {
+    const { scoreAssistance, scoreTakeClassAgain, scoreClarity } = vote;
+
     assistence += scoreAssistance * 20;
     classAgain += scoreTakeClassAgain * 20;
     clarity += scoreClarity * 20;
-  }
+  });
 
   assistence = Math.floor(assistence / votes.length);
   classAgain = Math.floor(classAgain / votes.length);
   clarity = Math.floor(clarity / votes.length);
 
-  return changeVariablesNames({ assistence, classAgain, clarity });
+  return {
+    scoreAssistance: assistence,
+    scoreTakeClassAgain: classAgain,
+    scoreClarity: clarity,
+  };
 };
 
 export default getRating;
