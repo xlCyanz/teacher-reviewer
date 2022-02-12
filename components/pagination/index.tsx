af7f19ba40a1e9c6a5d9d329a3e39d165a29f72a
xlCyanz <johanse.linares@gmail.com>
2022-02-11 23:40:53 -0400

    Updated pagination component.

--- components/pagination/index.tsx
+++ components/pagination/index.tsx
@@ -13,7 +13,7 @@ interface PaginationProps {
 }
 
 const Pagination = ({
-  totalCount, siblingCount = 1, pageSize, currentPage, changeCurrentPage,
+  totalCount, siblingCount = 0, pageSize, currentPage, changeCurrentPage,
 }: PaginationProps) => {
   const paginationRange = usePagination({
     currentPage,
@@ -23,7 +23,7 @@ const Pagination = ({
   });
 
   if (currentPage === 0 || paginationRange.length < 2) {
-    return <div className="">Error</div>;
+    return null;
   }
 
   const onNext = () => changeCurrentPage(currentPage + 1);
@@ -37,7 +37,7 @@ const Pagination = ({
         <button
           type="button"
           onClick={onPrev}
-          className="relative inline-flex items-center px-2 py-2 rounded-l-md bg-white text-sm font-medium text-gray-500 hover:bg-default-color hover:text-gray-100"
+          className="relative inline-flex items-center px-2 py-2 rounded-l-md bg-white dark:bg-gray-800 text-sm font-medium text-gray-500 hover:bg-default-color hover:text-gray-100"
         >
           <span className="sr-only">Previous</span>
           <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -46,7 +46,7 @@ const Pagination = ({
 
       {Children.toArray(_.map(paginationRange, (pageNumber) => {
         if (pageNumber === "...") {
-          return <div>...</div>;
+          return <div className="px-4 py-2 bg-white dark:bg-gray-800 dark:text-white">...</div>;
         }
 
         return (
@@ -54,10 +54,10 @@ const Pagination = ({
             type="button"
             aria-current={pageNumber === currentPage ? "page" : "false"}
             onClick={() => changeCurrentPage(typeof pageNumber === "string" ? 0 : pageNumber)}
-            className={`z-10 hidden sm:flex ${
+            className={`z-10 sm:flex ${
               pageNumber === currentPage
                 ? "bg-default-color border-default-color rounded text-gray-100"
-                : "bg-white text-gray-500 hover:bg-default-color hover:text-gray-100"
+                : "bg-white dark:bg-gray-800 text-gray-500 hover:bg-default-color hover:text-gray-100"
             } relative inline-flex items-center px-4 py-2 text-sm font-medium`}
           >
             {pageNumber}
@@ -69,7 +69,7 @@ const Pagination = ({
         <button
           type="button"
           onClick={onNext}
-          className="relative inline-flex items-center px-2 py-2 rounded-r-md bg-white text-sm font-medium text-gray-500 hover:bg-default-color hover:text-gray-100"
+          className="relative inline-flex items-center px-2 py-2 rounded-r-md bg-white dark:bg-gray-800 text-sm font-medium text-gray-500 hover:bg-default-color hover:text-gray-100"
         >
           <span className="sr-only">Next</span>
           <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
