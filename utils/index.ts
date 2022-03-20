569e1dd13fed3f8a32797cf039c349d0f81b9278
xlCyanz <johanse.linares@gmail.com>
2022-03-20 18:04:16 -0400

    New range util.

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,7 @@
 export { default as replaceJSX } from "./replace-jsx";
 export { default as connection } from "./connection";
 export { default as getRating } from "./get-rating";
-export { default as TimeSince } from "./time-since";
+export { default as timeSince } from "./time-since";
+export { default as range } from "./range";
 
 export * from "./add-type-name";
