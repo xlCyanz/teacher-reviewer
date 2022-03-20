25883e3fcdd60e265c193c968da9d494b9967a47
xlCyanz <johanse.linares@gmail.com>
2022-03-20 18:02:18 -0400

    Updated components.

--- components/index.ts
+++ components/index.ts
@@ -1,12 +1,10 @@
-export { default as Footer } from "./footer";
-export { default as Navigation } from "./navigation";
-export { default as VoteButton } from "./vote-button";
-export { default as CommentButton } from "./comment-button";
-export { default as CommentCard } from "./comment-card";
-export { default as Features } from "./features";
-export { default as TeacherCard } from "./teacher-card";
-export { default as Pagination } from "./pagination";
-export { default as DetailsCard } from "./details-card";
 export { default as Banner } from "./banner";
-
-export * from "./icons";
+export { default as Footer } from "./footer";
+export { default as Features } from "./features";
+export { default as Navigation } from "./navigation";
+export { default as Pagination } from "./pagination";
+export { default as VoteButton } from "./vote-button";
+export { default as CommentCard } from "./comment-card";
+export { default as TeacherCard } from "./teacher-card";
+export { default as DetailsCard } from "./details-card";
+export { default as CommentButton } from "./comment-button";
